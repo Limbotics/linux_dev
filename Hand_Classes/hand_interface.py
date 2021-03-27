9baf8c9b51546fbab14f7068d1591dc8ed70f96e
jered-b <jeredtbell@gmail.com>
2021-03-26 23:26:05 -0500

    Added another servo

--- Hand_Classes/hand_interface.py
+++ Hand_Classes/hand_interface.py
@@ -20,7 +20,7 @@ class grip_finger_angles(Enum):
     openGrip = {
         fingers.thumb.value:   0,
         fingers.index.value:   0,
-        # fingers.middle.value:  0,
+        fingers.middle.value:  0,
         # fingers.ring.value:    0,
         # fingers.pinky.value:   0
     }
@@ -28,7 +28,7 @@ class grip_finger_angles(Enum):
     closeGrip = {
         fingers.thumb.value:   0,
         fingers.index.value:   180,
-        # fingers.middle.value:  180,
+        fingers.middle.value:  180,
         # fingers.ring.value:    180,
         # fingers.pinky.value:   180
     }
@@ -36,7 +36,7 @@ class grip_finger_angles(Enum):
     pencil = {
         fingers.thumb.value:   150,
         fingers.index.value:   120,
-        # fingers.middle.value:  180,
+        fingers.middle.value:  180,
         # fingers.ring.value:    180,
         # fingers.pinky.value:   180
     }
@@ -44,7 +44,7 @@ class grip_finger_angles(Enum):
     cup = {
         fingers.thumb.value:   140,
         fingers.index.value:   160,
-        # fingers.middle.value:  160,
+        fingers.middle.value:  160,
         # fingers.ring.value:    160,
         # fingers.pinky.value:   160
     }
