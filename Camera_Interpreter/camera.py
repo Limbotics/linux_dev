1927dadd20d8ca72c559ed0324584cab5bdb8a0d
jered-b <jeredtbell@gmail.com>
2021-03-31 18:37:48 -0500

    test

--- Camera_Interpreter/camera.py
+++ Camera_Interpreter/camera.py
@@ -50,7 +50,7 @@ class camera_interface():
         self.cam_image = None
         self.cam_image_index = 0
         self.object_spotted_T0 = time.time()
-        self.object_not_spotted_delta_req = 3
+        self.object_not_spotted_delta_req = 10
 
     def camera_read_threader(self):
         #Start the read cam thread
