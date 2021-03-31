a9792e1f771dde192fb03844247a1b9ad537e4a0
jered-b <jeredtbell@gmail.com>
2021-03-31 18:35:58 -0500

    0 to 130

--- Hand_Classes/hand_interface.py
+++ Hand_Classes/hand_interface.py
@@ -59,7 +59,7 @@ class grip_finger_angles(Enum):
     }
 
     bottle_full_closed = {
-        fingers.thumb.value:   160,
+        fingers.thumb.value:   130,
         fingers.index.value:   160,
         fingers.middle.value:  160,
         fingers.ring.value:    160,
