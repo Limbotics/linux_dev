721b97ad20de494ec3eb5ac2e00b80794664305d
jered-b <jeredtbell@gmail.com>
2021-04-05 14:01:56 -0700

    Test

--- Camera_Interpreter/deep_learning_object_detection.py
+++ Camera_Interpreter/deep_learning_object_detection.py
@@ -15,7 +15,7 @@ args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+CLASSES = ["background","banana", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"]
