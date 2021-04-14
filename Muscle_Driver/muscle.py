0e2b388b95f69acd5f9d37318bcdc022f57c6cd7
jered-b <jeredtbell@gmail.com>
2021-04-14 13:43:05 -0700

    Testing

--- Muscle_Driver/muscle.py
+++ Muscle_Driver/muscle.py
@@ -27,7 +27,7 @@ class muscle_interface():
         self.fifoLength = 10                        #adjust to tune advanced trigger sensitvity
         self.fifo = queue.Queue(self.fifoLength)
 
-        self.analogThreshold = 12000
+        self.analogThreshold = 29000
         self.analogRatioThreshold = 3               #adjust to tune advanced trigger sensitvity
 
         self.off_buffer_delay = 0.75
