e65c2f586a002d2a5ce87bc9b2a699069f222540
Mark Wu <markwu.ca@gmail.com>
2021-04-24 01:42:02 -0700

    Update testing_main.py
    making less verbose

--- testing_main.py
+++ testing_main.py
@@ -9,7 +9,7 @@ mi = muscle.muscle_interface()
 while True:
     if mi.peakTriggered():
         print("triggered!!!!!!!!!")
-    else:
-        print("not triggered")
+    # else:
+        # print("not triggered")
     # print(mi.bufferList)
     print(mi.peaks[0])
