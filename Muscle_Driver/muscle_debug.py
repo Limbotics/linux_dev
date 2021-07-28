c166e962c9b3ff32c1367e668998e4e12ff5ab3f
Jered Bell <jbell19@calpoly.edu>
2021-07-27 21:01:00 -0700

    Debug

--- Muscle_Driver/muscle_debug.py
+++ Muscle_Driver/muscle_debug.py
@@ -9,6 +9,8 @@ class MyService(rpyc.Service):
         self.channel_0 = 0
         self.channel_1 = 0
 
+        print("init completed")
+
     # My service
     def exposed_echo(self, text):
         print(text)
@@ -17,16 +19,23 @@ class MyService(rpyc.Service):
         return self.channel_0
 
     def exposed_channel_1_value(self):
-        return self.channel_1        
+        return self.channel_1       
+
+# def main_thread():
+#     while(True):
+#         print("Hello! Press 1 to send a down pulse, 2 for a down hold (1 second), 3 for up hold (1 second)")
+#         ans = input()
+#         if ans == 1:
+#             channel_0 = 10000
+#             time.sleep(0.3)
+#             channel_0 = 0
 
 if __name__ == "__main__":
+
+    # UI_thread = threading.Thread(main_thread, args=())
+    # UI_thread.start()
+
     server = ThreadedServer(MyService, port = 18812)
     server.start()
 
-    while(True):
-            print("Hello! Press 1 to send a down pulse, 2 for a down hold (1 second), 3 for up hold (1 second)")
-            ans = input()
-            if ans == 1:
-                self.channel_0 = 10000
-                time.sleep(0.3)
-                self.channel_0 = 0
+    
