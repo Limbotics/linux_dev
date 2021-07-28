dfee888822405d5912c1d47257d29c68d477ca46
Jered Bell <jbell19@calpoly.edu>
2021-07-27 20:19:18 -0700

    Test

--- Muscle_Driver/muscle_debug.py
+++ Muscle_Driver/muscle_debug.py
@@ -5,7 +5,11 @@ class MyService(rpyc.Service):
     # My service
     def exposed_echo(self, text):
         print(text)
+        
+    def test_func_def(self, text):
+        print(text)
 
+        
 if __name__ == "__main__":
     server = ThreadedServer(MyService, port = 18812)
     server.start()
