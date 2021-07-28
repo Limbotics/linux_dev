7c45a5fd6e155b2fb70868f0ca769030118fa7bf
Jered Bell <jbell19@calpoly.edu>
2021-07-27 20:23:00 -0700

    Debug

--- Muscle_Driver/muscle_debug.py
+++ Muscle_Driver/muscle_debug.py
@@ -2,13 +2,13 @@ import rpyc
 from rpyc.utils.server import ThreadedServer
 
 class MyService(rpyc.Service):
+
     # My service
     def exposed_echo(self, text):
         print(text)
         
-    def exposed_test_func_def(self, text):
-        print(text)
-
+    def exposed_double_me(self, input_num):
+        return 2*input_num
 
 if __name__ == "__main__":
     server = ThreadedServer(MyService, port = 18812)
