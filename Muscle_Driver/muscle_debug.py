3765d5346bb9d4388c556123343110fec94b7466
Jered Bell <jbell19@calpoly.edu>
2021-07-27 20:46:10 -0700

    Test human input into server

--- Muscle_Driver/muscle_debug.py
+++ Muscle_Driver/muscle_debug.py
@@ -1,14 +1,35 @@
 import rpyc
 from rpyc.utils.server import ThreadedServer
+import threading
+import time
 
 class MyService(rpyc.Service):
 
+    def __init__(self):
+        self.channel_0 = 0
+        self.channel_1 = 0
+
+        #Start the user interface thread
+        self.main_thread()
+        
     # My service
     def exposed_echo(self, text):
         print(text)
         
-    def exposed_double_me(self, input_num):
-        return 2*input_num
+    def exposed_channel_0_value(self):
+        return self.channel_0
+
+    def exposed_channel_1_value(self):
+        return self.channel_1
+
+    def main_thread(self):
+        while(True):
+            print("Hello! Press 1 to send a down pulse, 2 for a down hold (1 second), 3 for up hold (1 second)")
+            ans = input()
+            if ans == 1:
+                self.channel_0 = 10000
+                time.sleep(0.3)
+                self.channel_0 = 0
 
 if __name__ == "__main__":
     server = ThreadedServer(MyService, port = 18812)
