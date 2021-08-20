8f0011dd67918ed3573641e8361da56af12466ad
jered-b <80386623+jered-b@users.noreply.github.com>
2021-08-19 23:00:21 -0700

    big meme

--- stats.py
+++ stats.py
@@ -54,7 +54,7 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype('/home/mendel/new_dev/linux_dev/monofonto.otf', 20)
+font = ImageFont.truetype('/home/mendel/new_dev/linux_dev/monofonto.otf', 10)
 
 while True:
 
