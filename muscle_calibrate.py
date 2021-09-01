c7114141f95a37c1cc688c9b03cd511db019e4d3
jered-b <80386623+jered-b@users.noreply.github.com>
2021-09-01 15:50:11 -0700

    Added user prompt to sensor debug

--- muscle_calibrate.py
+++ muscle_calibrate.py
@@ -1,6 +1,7 @@
 from Muscle_Driver import muscle
 
 mi = muscle.muscle_interface()
-
+print("What channel do you want to read?")
+ans = int(input())
 while True:
-    print(str(mi.ads.read_adc(1, gain=1)))
+    print(str(mi.ads.read_adc(ans, gain=1)))
