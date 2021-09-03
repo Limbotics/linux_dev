f8d6330d1fc5450878dc3c7f6903b820803ace20
jered-b <80386623+jered-b@users.noreply.github.com>
2021-09-02 20:48:26 -0700

    Cam only

--- cam_only.py
+++ cam_only.py
@@ -30,38 +30,38 @@ cam = camera.camera_interface()
 
 
 #Muscle sensor initialization
-print("Debug muscle sensor? Y/N")
-ans = "Y"
-if(ans == "Y"):
-    mi = muscle.muscle_interface(disconnect=True)
-else:
-    mi = muscle.muscle_interface()
+# print("Debug muscle sensor? Y/N")
+# ans = "Y"
+# if(ans == "Y"):
+#     mi = muscle.muscle_interface(disconnect=True)
+# else:
+#     mi = muscle.muscle_interface()
 
-    #Run through the muscle calibration sequence if necessary
-    if not mi.disconnected:
+#     #Run through the muscle calibration sequence if necessary
+#     if not mi.disconnected:
 
-        # print("[CALIBRATION] Logging current channel 0 input as input threshold in 3...")
-        # time.sleep(1)
-        # print("2...")
-        # time.sleep(1)
-        # print("1...")
-        # time.sleep(1)
-        mi.update_0_threshold()
-        # print("[CALIBRATION-CH0] Please perform a down squeeze, starting on 2, until 0.")
-        # print("[CALIBRATION-CH0] Ready?")
-        # time.sleep(2)
-        # print("3...")
-        # time.sleep(1)
-        # print("Squeeze now, and hold! 2...")
-        # time.sleep(1)
-        # print("1...")
-        mi.update_0_max()
-        # time.sleep(1) Not needed due to the integral/interval method in above function
-        # print("[CALIBRATION-CH0] Great! Thank you.")
-        # print("[CALIBRATION] Calibration sequence summary:")
-        # print("[CAL-CH0] CH0 input threshold: ", str(mi.analogThreshold_0), "CH0 max: ", str(mi.max_input_0))
-        # time.sleep(2)
-        # print("[CAL-CH1] CH1 input threshold: ", str(mi.analogThreshold_1), "CH1 max: ", str(mi.max_input_1))
+#         # print("[CALIBRATION] Logging current channel 0 input as input threshold in 3...")
+#         # time.sleep(1)
+#         # print("2...")
+#         # time.sleep(1)
+#         # print("1...")
+#         # time.sleep(1)
+#         mi.update_0_threshold()
+#         # print("[CALIBRATION-CH0] Please perform a down squeeze, starting on 2, until 0.")
+#         # print("[CALIBRATION-CH0] Ready?")
+#         # time.sleep(2)
+#         # print("3...")
+#         # time.sleep(1)
+#         # print("Squeeze now, and hold! 2...")
+#         # time.sleep(1)
+#         # print("1...")
+#         mi.update_0_max()
+#         # time.sleep(1) Not needed due to the integral/interval method in above function
+#         # print("[CALIBRATION-CH0] Great! Thank you.")
+#         # print("[CALIBRATION] Calibration sequence summary:")
+#         # print("[CAL-CH0] CH0 input threshold: ", str(mi.analogThreshold_0), "CH0 max: ", str(mi.max_input_0))
+#         # time.sleep(2)
+#         # print("[CAL-CH1] CH1 input threshold: ", str(mi.analogThreshold_1), "CH1 max: ", str(mi.max_input_1))
 
 #Servo control initialization
 servs = servo.handLUTControl()
@@ -100,7 +100,7 @@ while (cam_thread.is_alive() and not SM.killed):
 
     #Create new state matrix for current moment
     reported_object = cam.cam_data
-    user_command_detected = mi.AnalogRead()
+    # user_command_detected = mi.AnalogRead()
 
     #Set grip_picked to "" if it's not in the database of known objects
     object_id = True
@@ -121,15 +121,15 @@ while (cam_thread.is_alive() and not SM.killed):
         data_list["other_cam_data"] = cam.other_cam_data 
         data_list["inference_time"] = str(round((1/cam.inference_time), 1))
         data_list["spotted_object_score"] = str(round((100*cam.cam_data_score), 2))
-        data_list["EMG_array"] = mi.averaging_array
+        data_list["EMG_array"] = ""
         try:
-            data_list["smoothing_time"] = str(mi.smoothing_time)
-            data_list["muscle_input"] = str(int(mi.filtered_data[-1]))
+            data_list["smoothing_time"] = ""
+            data_list["muscle_input"] = ""
         except Exception as e:
             data_list["muscle_input"] = str("N/A")
             data_list["smoothing_time"] = str("N/A")
-        data_list["muscle_input_percent"] = str(100*mi.last_input[1])
-        data_list["muscle_input_type"] = str(mi.last_input[0])
+        data_list["muscle_input_percent"] = ""
+        data_list["muscle_input_type"] = ""
         data_list["servo_grip_loaded"] = str(grip_name)
         # data_list["vibes"] = str(statuslights.vibe_status)
         
@@ -166,7 +166,7 @@ while (cam_thread.is_alive() and not SM.killed):
 # except KeyboardInterrupt:
 print("\nScript quit command detected - closing IO objects.")
 # statuslights.startup_complete = False
-mi.shutdown()
+# mi.shutdown()
 
 cam.end_camera_session()
 # cam_thread.join() #Don't continue until the thread is closed 
