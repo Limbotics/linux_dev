fae662148b4ef646f6f31fce30572340ef0642fe
jered-b <80386623+jered-b@users.noreply.github.com>
2021-09-02 23:55:55 -0700

    Autosize pic?

--- Camera_Interpreter/camera.py
+++ Camera_Interpreter/camera.py
@@ -243,10 +243,10 @@ class camera_interface():
 
         cv2.namedWindow(name)
         cv2.moveWindow(name, 900,-900)
-        cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        cv2.namedWindow("window", cv2.WINDOW_AUTOSIZE)
+        # cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
         cv2.imshow(name, img)
-        # cv2.waitKey(1)
+        cv2.waitKey(1)
         return
 
     def line_length(self, x0, x1, y0, y1):
