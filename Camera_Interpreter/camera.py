3daa24c5d991bbcb352c31ce54a2935ff58ba38a
jered-b <80386623+jered-b@users.noreply.github.com>
2021-09-02 23:56:41 -0700

    Fix?

--- Camera_Interpreter/camera.py
+++ Camera_Interpreter/camera.py
@@ -243,7 +243,7 @@ class camera_interface():
 
         cv2.namedWindow(name)
         cv2.moveWindow(name, 900,-900)
-        cv2.namedWindow("window", cv2.WINDOW_AUTOSIZE)
+        cv2.namedWindow(name, cv2.WINDOW_AUTOSIZE)
         # cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
         cv2.imshow(name, img)
         cv2.waitKey(1)
